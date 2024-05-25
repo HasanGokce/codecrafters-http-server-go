6704ffaaf52d46edd0a08a66905e4f37bdbf871c
Hasan Gökçe <smallsteps@Hasan-Air.home>
2024-05-25 18:50:38 +0300

    pass stage

--- app/server.go
+++ app/server.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
-	"net"
-	"os"
+	"net/http"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "HTTP/1.1 200 OK\r\n\r\n")
+
+	})
+
+	err := http.ListenAndServe(":4221", nil)
 	if err != nil {
-		fmt.Println("Failed to bind to port... 4221")
-		os.Exit(1)
+		fmt.Println("Failed to start server on port 4221:", err)
 	}
 
-	for {
-		_, err = l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-	}
 }
